7727e545f49270fa8b882c1410e666cc1ea3e399
dan liu <2831479724@qq.com>
2023-06-07 18:39:39 +0800

    add model

---
+++ model/encoder.py
@@ -0,0 +1,20 @@
+"""
+encoder of vae
+create by liudan
+
+"""
+
+import torch
+import torch.nn as nn
+
+class encoder(nn.Module):
+    """
+    input : batchsize x 3 x 64 x 64 
+    output: 
+    """
+    def __init__(self):
+        super.__init__()
+
+        
+
+
